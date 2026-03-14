a3ad8fba316d719418b7d9732e4c5c1afe0fcb45
A.Prokopenko <psevdoproger@gmail.com>
2026-03-14 19:24:33 +0000

    test: increase testTimeout to 30s for bundler-heavy tree-shaking tests

--- packages/react/vitest.config.ts
+++ packages/react/vitest.config.ts
@@ -14,6 +14,7 @@ export default defineConfig({
 	test: {
 		globals: true,
 		environment: 'node',
+		testTimeout: 30000,
 		include: ['test/**/*.{ts,tsx}'],
 		exclude: ['test/helpers/**', 'test/fixtures/**'],
 		pool: 'forks',
